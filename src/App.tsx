6d186d6bb95dd361a1359a9727e2bc265180ddd6
Jamie-McCaw <jamie.b.mccaw@gmail.com>
2021-05-11 20:54:11 -0400

    Init with Tailwind/Typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-blue-500">
+          TailwindCSS setup
+      </div>
+    </div>
+  );
+}
+
+export default App;
