770de17aa8132b159c826882523534e0c704ae52
David Hatcher <sallazar024@gmail.com>
2021-05-12 13:20:03 -0400

    Testing to ensure tailwind working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './css/App.css';
 
 function App() {
+  const [toggle,setToggle] = useState<Boolean>(true);
+  
+  const getColor = (toggleState: Boolean): string => {
+    if(toggleState) {
+      return "bg-white hover:bg-gray-100 text-gray-800";
+    }else{
+      return "bg-gray-800 hover:bg-gray-100 text-gray-100 hover:text-black";
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid grid-rows-5 grid-cols-5 h-full">
+      <button className={`${getColor(toggle)} font-semibold py-2 px-4 border border-gray-400 rounded shadow row-start-3 col-start-3`} onClick={(e) => setToggle(!toggle)}>Hello Toggle</button>
     </div>
   );
 }
