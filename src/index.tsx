0f720fee296f1cc2786b440bebe891f7db7f30fc
David Hatcher <sallazar024@gmail.com>
2021-05-12 13:00:16 -0400

    Testing with new Deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/index.css';
 import App from './App';
-import './styles/tailwind.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
